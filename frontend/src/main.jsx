bfa2393cf3ccccdd8323767a64438c0740590234
abhishekkumar2770 <abhishekk27033@gmail.com>
2025-12-26 15:26:24 +0530

    new change

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css"; // ✅ This must be present
+import axios from "axios";
+
+// ✅ Enable Cookies for all requests globally
+axios.defaults.withCredentials = true;
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
 
